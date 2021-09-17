88ab658516b1b88d8f59ed862d50966ae8019cb2
Nibuyaga <nibuyaga1@gmail.com>
2021-09-17 13:49:52 +0200

    test 17-9-2021

---
+++ Project_MV_Vb/Objects/Player/Test_Block_David.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+
+var move = Vector2(0,0)
+export var Speed = 180
+export var Gravity = 10
+export var JumpForce = -500
+
+onready var drop_check = get_node("Drop_Down_Check")
+
+func _input(event):
+	
+#	input om door de "pass" tiles te vallen
+	if event.is_action_pressed("ui_down") and drop_check.is_colliding():
+		position.y += 1
+
+func _physics_process(delta):
+	
+	move.x = Speed * (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
+	move.y += Gravity
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		move.y = JumpForce
+	
+	move = move_and_slide(move, Vector2.UP)
+	
+	move.x = lerp(move.x, 0, 0.2)
+
