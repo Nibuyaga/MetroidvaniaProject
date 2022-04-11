d4df608856aece2055f17ccb5b150f8fcc7efd3f
Nibuyaga <nibuyaga1@gmail.com>
2022-04-11 11:33:42 +0200

    Added Door Object

---
+++ Project_MV_Vb/Rooms/Area01/Area01 - Objects/Door - Area01.gd
@@ -0,0 +1,49 @@
+extends StaticBody2D
+
+
+var listKeys = [
+	"NormalBullet"
+]
+var closeDirectly = true
+
+export var flip_h = false
+
+
+
+func _ready():
+	if flip_h:
+		$Sprite.set("flip_h", true)
+	
+
+
+
+func open_coll():
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Area2D.set_deferred("monitoring", false)
+
+func close_coll():
+	$CollisionShape2D.set_deferred("disabled", false)
+	$Area2D.set_deferred("monitoring", true)
+
+
+func _on_Area2D_area_entered(area):
+	
+	# Does something when Node name is in listKeys
+	if area.get_name() in listKeys:
+		open_coll()
+		$AnimationPlayer.play("Open")
+
+
+func _on_Player_Detect_body_entered(body):
+	closeDirectly = false
+
+
+func _on_Player_Detect_body_exited(body):
+	# Start a closing Timer
+	pass
+
+
+func _on_Timer_InstantClose_timeout():
+	if closeDirectly == true:
+		close_coll()
+		$AnimationPlayer.play("Instant Close")
