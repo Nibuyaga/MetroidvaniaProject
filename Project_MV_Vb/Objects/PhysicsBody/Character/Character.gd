89c93f68044a44125a28a2fc3f0b924ebf2f8555
Nibuyaga <nibuyaga1@gmail.com>
2022-05-24 13:47:01 +0200

    work update, 24-05-2022

--- Project_MV_Vb/Objects/PhysicsBody/Character/Character.gd
+++ Project_MV_Vb/Objects/PhysicsBody/Character/Character.gd
@@ -10,10 +10,33 @@ export var stats = {
 	'alive': true,
 	
 }
+export var invincibility = 0
+var invi_timer = 0
 
+var damage_invincibility = false
 var facing = 0
 var is_jumping = false
 
+#may need expanding if wanting to use status
+var envi_damage = {
+	"state": false	,
+	"damage tick": 1,
+	"cooldown set": 1,
+	"cooldown current": 0
+}
+
+func _process(delta):
+	
+	
+	# invincibility timer
+	if damage_invincibility:
+		invi_timer -= delta
+		if invi_timer <= invincibility:
+			damage_invincibility = false
+	
+	# environment damage
+	if envi_damage["state"] or envi_damage["cooldown current"] > 0:
+		envi_handling(delta)
 
 # Default jump function, overwrite in subclasses
 func jump():
@@ -34,18 +57,42 @@ func _on_Hurtbox_area_entered(area):
 	else:
 		knockback(Vector2(1000,-100))
 
-	if "damage" in area:	# This needs to be tested
+	if "damage" in area:
 		calc_health(area.damage)
 	else:
 		calc_health(1)
 
 
-func calc_health(damage):
+func _on_Hurtbox__Environment_body_entered(_body):
+	envi_damage["state"] = true
+
+func _on_Hurtbox__Environment_body_exited(_body):
+	envi_damage["state"] = false
+
+func envi_handling(delta):
+	if envi_damage["state"] == true:
+		if envi_damage["cooldown current"] <= 0:
+			calc_health(envi_damage["damage tick"])
+			envi_damage["cooldown current"] = envi_damage["cooldown set"]
+		else:
+			envi_damage["cooldown current"] -= delta
+	else:
+		envi_damage["cooldown current"] -= delta
+
+#var envi_damage = {
+#	"state": false	,
+#	"damage tick": 1,
+#	"cooldown set": 1,
+#	"cooldown current": 0
+#}
+
+func calc_health(damage, animation = true):
 	if stats['alive']:
 		stats['health'] -= damage
+		print(self.name + "'s health: " + str(stats["health"]))
 		if stats['health'] <= 0:
 			die()
-		else:
+		elif animation:
 			$AnimationPlayer.play("hurt")
 
 func die():
@@ -58,3 +105,4 @@ func die():
 
 func at_dead_queue_free():
 	queue_free()
+
