13ab10c8251322430b972318158f855d9e2e4d54
henderikjahan <momojohobo@rocketship.com>
2022-07-11 11:25:57 +0200

    don't allow walljumping up a single wall

--- Project_MV_Vb/Objects/PhysicsBody/Character/Player/Player.gd
+++ Project_MV_Vb/Objects/PhysicsBody/Character/Player/Player.gd
@@ -12,6 +12,8 @@ var jumping = false
 var initial_jump = false
 var real_air_drag = air_drag
 var wall_jump_air_drag = 0.8
+var last_facing_wall = 0
+var wall_jump_reset = 0
 
 var aim = 0 # multiply by 45 degrees
 var ani_aim = 0
@@ -131,10 +133,11 @@ func jump():
 		# 200 being the fall-speed limit.
 		if is_on_floor() or (initial_jump and velocity.y <= 200 and velocity.y >= 0):
 			velocity.y = -jumpforce
-		elif on_wall and (stats['wall_jump'] == true):
+		elif on_wall and (stats['wall_jump'] == true) and not last_facing_wall == facing:
 			velocity.x -= (on_wall * wall_jump_force)*2.5
 			velocity.y = -jumpforce/1.5
 			facing = -on_wall
+			last_facing_wall = -facing
 			# temporary air_drag when wall jumping 
 			air_drag = wall_jump_air_drag
 			var dash = dash_vfx.instance()
@@ -186,7 +189,11 @@ func animate():
 	if on_wall and not is_on_floor():
 		$AnimationTree.set("parameters/movement/current", 5)
 
-func wall_slide():
+func wall_slide(delta):
+	wall_jump_reset += delta
+	if wall_jump_reset > 1:
+		wall_jump_reset = 0
+		last_facing_wall = 0
 	var space_state = get_world_2d().direct_space_state
 	var to = global_position + Vector2(facing * wall_jump_distance, 0)
 	if len(space_state.intersect_ray(global_position, to, [self])) > 0:
@@ -203,11 +210,13 @@ func _process(delta):
 		facing = horizontal_movement
 		velocity.x += speed * facing * delta
 	if is_on_floor():
+		last_facing_wall = 0
+		wall_jump_reset = 0
 		air_drag = real_air_drag
 		stats['double_jump'] = 0
 		initial_jump = true
 	else:
-		wall_slide()
+		wall_slide(delta)
 	if Input.is_action_pressed('jump'):
 		jump()
 	else:
