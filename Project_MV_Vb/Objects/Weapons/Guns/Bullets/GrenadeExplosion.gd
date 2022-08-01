06ca426ef7df8a4566e36ceb39cbf6219aca6e89
henderikjahan <momojohobo@rocketship.com>
2022-08-01 14:11:40 +0200

    play soundfx

--- Project_MV_Vb/Objects/Weapons/Guns/Bullets/GrenadeExplosion.gd
+++ Project_MV_Vb/Objects/Weapons/Guns/Bullets/GrenadeExplosion.gd
@@ -2,18 +2,17 @@ extends Area2D
 
 
 # Declare member variables here. Examples:
-export var timer = 0.2
+export var timer = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$grenade_explosion.play()
 
 func _process(delta):
 	timer -= delta
 	if timer <= 0:
 		queue_free()
-	
+
 	# Tile destruction
 	if len(get_overlapping_bodies()) > 0:
 		$TDfunc.TD_multibodies(get_overlapping_bodies(), self)
--- Project_MV_Vb/Objects/Weapons/Guns/gun_grenade.gd
+++ Project_MV_Vb/Objects/Weapons/Guns/gun_grenade.gd
@@ -17,3 +17,4 @@ func update_weapon(delta, aim, attack=false):
 		bullet.rotation_degrees = aim*45
 		Global.grab_current_level().add_child(bullet)
 		bullet.velocity = bullet.velocity.rotated(deg2rad((aim-2)*45))
+		$lop.play()
